cf6b5e2fea4de1cb0aad1b6807cd16866bfce674
joshuadavidang <joshuadavidang@outlook.sg>
2024-02-17 18:42:28 +0800

    feat: add in sonner component

--- app/hooks/useUserData.ts
+++ app/hooks/useUserData.ts
@@ -1,15 +1,24 @@
 import { fetcher } from "@/api/helper";
 import { BACK_END_API_URL } from "@/utils/constants";
 import { type UserData, type UserInfo } from "@/utils/userInfo";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
 export const useUserData = (): UserData => {
+  const router = useRouter();
   const [user, setUser] = useState<UserInfo | null>(null);
   const { data, error, isLoading } = useSWR<UserInfo>(
     `${BACK_END_API_URL}/${process.env.NEXT_PUBLIC_USER_DATA}`,
     fetcher
   );
+
+  useEffect(() => {
+    if (data?.code === 440) {
+      alert("Session expired!");
+      router.push("/");
+    }
+  }, [data, router]);
 
   useEffect(() => {
     if (!isLoading && !error && data && data.code === 200) {
