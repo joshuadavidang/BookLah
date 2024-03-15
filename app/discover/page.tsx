d932b7dac5e80461242abe777ae15e61b42066ba
joshuadavidang <joshuadavidang@outlook.sg>
2024-03-15 09:45:12 +0800

    fix: on mount bug
    Signed-off-by: joshuadavidang <joshuadavidang@outlook.sg>

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -8,7 +8,7 @@ import { UserType } from "@/types/concertDetails";
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
-export default function Discover() {
+export const Discover = () => {
   const path = usePathname();
   const smoothScroll = path === "/discover";
 
@@ -25,13 +25,15 @@ export default function Discover() {
   const isAdmin = localStorage.getItem("userType") === UserType.ADMIN;
 
   return (
-    <ProtectComponent>
+    <>
       <div className="hidden absolute top-10 right-10 justify-end lg:flex w-screen">
         <Button variant="link" size="lg" className="text-gray-100">
           {name}
         </Button>
       </div>
       {isAdmin ? <Admin /> : <Concert />}
-    </ProtectComponent>
+    </>
   );
-}
+};
+
+export default ProtectComponent(Discover);
