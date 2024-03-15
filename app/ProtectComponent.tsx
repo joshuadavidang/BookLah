97ec461bd2143274f5fbf7be548080c3885b814a
Joshua David <joshuadavidang@outlook.sg>
2024-03-15 09:45:48 +0800

    fix: on mount bug (#23)
    Signed-off-by: joshuadavidang <joshuadavidang@outlook.sg>

--- app/ProtectComponent.tsx
+++ app/ProtectComponent.tsx
@@ -9,46 +9,44 @@ import useSWR from "swr";
 import LoadingIndicator from "./components/Loading";
 import { UserType } from "./types/concertDetails";
 
-interface ProtectComponentProps {
-  children: React.ReactNode;
-}
+export const ProtectComponent = (WrappedComponent: any) => {
+  return function ProtectComponent(props: any) {
+    const {
+      data: userData,
+      error,
+      isLoading,
+    } = useSWR<UserInfo>(
+      `${BACK_END_API_URL}/${process.env.NEXT_PUBLIC_USER_DATA}`,
+      fetcher
+    );
 
-export const ProtectComponent = ({ children }: ProtectComponentProps) => {
-  const {
-    data: userData,
-    error,
-    isLoading,
-  } = useSWR<UserInfo>(
-    `${BACK_END_API_URL}/${process.env.NEXT_PUBLIC_USER_DATA}`,
-    fetcher
-  );
+    useEffect(() => {
+      if (userData && userData.code === 200) {
+        const isAdmin = userData.userData.data.userType === UserType.ADMIN;
+        const name = userData.userData.data["myinfo.name"] || "Guest";
 
-  useEffect(() => {
-    if (userData && userData.code === 200) {
-      const isAdmin = userData.userData.data.userType === UserType.ADMIN;
-      const name = userData.userData.data["myinfo.name"] || "Guest";
+        localStorage.setItem("user", userData.userData.sub);
+        localStorage.setItem(
+          "userType",
+          isAdmin ? UserType.ADMIN : UserType.USER
+        );
+        localStorage.setItem("name", name);
 
-      localStorage.setItem("user", userData.userData.sub);
-      localStorage.setItem(
-        "userType",
-        isAdmin ? UserType.ADMIN : UserType.USER
-      );
-      localStorage.setItem("name", name);
+        const dataObj = {
+          userId: userData.userData.sub,
+          name: name,
+          gender: "Male",
+          userType: isAdmin ? UserType.ADMIN : UserType.USER,
+        };
 
-      const dataObj = {
-        userId: userData.userData.sub,
-        name: name,
-        gender: "Male",
-        userType: isAdmin ? UserType.ADMIN : UserType.USER,
-      };
+        const apiURL = `${BACK_END_API_URL}/${process.env.NEXT_PUBLIC_SAVE_USER_INFORMATION}`;
+        backendAxiosPost(apiURL, dataObj);
+      }
+    }, [userData]);
 
-      const apiURL = `${BACK_END_API_URL}/${process.env.NEXT_PUBLIC_SAVE_USER_INFORMATION}`;
-      backendAxiosPost(apiURL, dataObj);
-    }
-  }, [userData]);
+    if (error) return redirect(LANDING_URL);
+    if (isLoading) return <LoadingIndicator />;
 
-  if (isLoading) return <LoadingIndicator />;
-  if (error) return redirect(LANDING_URL);
-
-  return <>{children}</>;
+    return <WrappedComponent {...props} />;
+  };
 };
