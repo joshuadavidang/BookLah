0d7a184dea43ff1f55296194fb3636d26922d5bd
Joshua David <joshuadavidang@outlook.sg>
2024-03-19 20:22:18 +0800

    feat: add order details (#33)
    Signed-off-by: joshuadavidang <joshuadavidang@outlook.sg>

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -2,5 +2,6 @@ const BACK_END_API_URL = process.env.NEXT_PUBLIC_API_ENDPOINT;
 const LANDING_URL = "/";
 const DISCOVER_URL = "/discover";
 const FORM_URL = "/form";
+const ORDERS_URL = "/orders";
 
-export { BACK_END_API_URL, DISCOVER_URL, FORM_URL, LANDING_URL };
+export { BACK_END_API_URL, DISCOVER_URL, FORM_URL, LANDING_URL, ORDERS_URL };
